4e67b59eb1c87d8a0a9e76589de8ad843d964f56
yangkun <yangkun>
2021-08-01 20:57:51 +0800

    router-link

--- src/components/common/menu.vue
+++ src/components/common/menu.vue
@@ -1,39 +1,33 @@
 <template>
-    <div>
-        <!-- <el-row>   -->
-            <!-- <el-col> -->
-              <el-menu
-                default-active="1"
-                class="el-menu-vertical-demo"
-                background-color="#324057"
-                text-color="#fff"
-                active-text-color="#ffd04b">
-                <el-menu-item v-for="(item,index) in menu" :key="index">
-                  <template slot="title">
-                      <i class="el-icon-menu"></i>
-                      <router-link :to="item.index" style="width: 100%;color: #fff;text-decoration: none;">{{item.title}}</router-link>  
-                    </template>
-                </el-menu-item>
-              </el-menu>
-            <!-- </el-col> -->
-          <!-- </el-row> -->
-    </div>
+  <div>
+    <el-menu default-active="1" class="el-menu-vertical-demo" background-color="#324057" text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-menu-item v-for="(item,index) in menu" :key="index">
+        <template slot="title">
+          <router-link :to="item.index" style="width: 100%;color: #fff;text-decoration: none;display: inline-block;">
+            <i class="el-icon-menu"></i>
+            {{item.title}}
+          </router-link>
+        </template>
+      </el-menu-item>
+    </el-menu>
+  </div>
 </template>
 
 <script>
   export default {
     name: 'Menu',
-    data(){
-        return{
-           menu:[]
-        }
+    data() {
+      return {
+        menu: []
+      }
     },
-    created(){
+    created() {
       this.menu = JSON.parse(this.$store.state.userInfo.enterprise.menu)
       console.log(this.menu)
     },
-    mounted(){
-      
+    mounted() {
+
     }
   }
 </script>
@@ -41,4 +35,3 @@
 <style scoped>
 
 </style>
-
